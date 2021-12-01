d53d3fc1afa0895c272ee17bb9e490d227f409be
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-12-01 08:00:07 -0600

    set up useState

--- src/EightBall.js
+++ src/EightBall.js
@@ -1,34 +1,55 @@
-import './src/EightBall.css';
+import React, { useState } from "react";
+import './EightBall.css';
 
-const answers = [
-    { msg: "It is certain.", color: "green" },
-    { msg: "It is decidedly so.", color: "green" },
-    { msg: "Without a doubt.", color: "green" },
-    { msg: "Yes - definitely.", color: "green" },
-    { msg: "You may rely on it.", color: "green" },
-    { msg: "As I see it, yes.", color: "green" },
-    { msg: "Most likely.", color: "green" },
-    { msg: "Outlook good.", color: "green" },
-    { msg: "Yes.", color: "green" },
-    { msg: "Signs point to yes.", color: "goldenrod" },
-    { msg: "Reply hazy, try again.", color: "goldenrod" },
-    { msg: "Ask again later.", color: "goldenrod" },
-    { msg: "Better not tell you now.", color: "goldenrod" },
-    { msg: "Cannot predict now.", color: "goldenrod" },
-    { msg: "Concentrate and ask again.", color: "goldenrod" },
-    { msg: "Don't count on it.", color: "red" },
-    { msg: "My reply is no.", color: "red" },
-    { msg: "My sources say no.", color: "red" },
-    { msg: "Outlook not so good.", color: "red" },
-    { msg: "Very doubtful.", color: "red" }
-];
+function choice(msgs) {
+    const randomIdx = Math.floor(Math.random() * msgs.length);
+    return msgs[randomIdx];
+}
 
-const EightBall = ({ answers }) => {
+function EightBall(props) {
+    const [msg, setMsg] = useState("Think of a Question");
+    const [color, setColor] = useState("black");
+
+    function handleClick() {
+        const {msg, color} = choice(props.answers);
+        setMsg(msg);
+        setColor(color);
+    }
+
     return (
-        <>
-        <h1>Think of a Question.</h1>
-        </>
+        <div
+        className="EightBall"
+        onClick={handleClick}
+        style={{ backgroundColor: color }
+        }>
+            <b>{msg}</b>
+        </div>
     );
 }
+
+EightBall.defaultProps = {
+    answers: [
+        { msg: "It is certain.", color: "green" },
+        { msg: "It is decidedly so.", color: "green" },
+        { msg: "Without a doubt.", color: "green" },
+        { msg: "Yes - definitely.", color: "green" },
+        { msg: "You may rely on it.", color: "green" },
+        { msg: "As I see it, yes.", color: "green" },
+        { msg: "Most likely.", color: "green" },
+        { msg: "Outlook good.", color: "green" },
+        { msg: "Yes.", color: "green" },
+        { msg: "Signs point to yes.", color: "goldenrod" },
+        { msg: "Reply hazy, try again.", color: "goldenrod" },
+        { msg: "Ask again later.", color: "goldenrod" },
+        { msg: "Better not tell you now.", color: "goldenrod" },
+        { msg: "Cannot predict now.", color: "goldenrod" },
+        { msg: "Concentrate and ask again.", color: "goldenrod" },
+        { msg: "Don't count on it.", color: "red" },
+        { msg: "My reply is no.", color: "red" },
+        { msg: "My sources say no.", color: "red" },
+        { msg: "Outlook not so good.", color: "red" },
+        { msg: "Very doubtful.", color: "red" }
+    ]
+};
 
 export default EightBall;
